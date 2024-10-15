9788a1501428bd0e2c1400c3646479c47c15775a
Shahram <shahramdeveloper11@gmail.com>
2024-10-15 09:45:05 +0330

    Vazir font add to MaterialApp theme in Application widget in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,24 @@ class Application extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'vazir'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('سنگ   کاغذ   قیچی'),
+          centerTitle: true,
+          backgroundColor: Colors.deepOrangeAccent,
+        ),
+        backgroundColor: Colors.deepOrangeAccent,
+        body: SafeArea(
+          child: Column(
+            children: [
+              Text('Shahram Academy'),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
